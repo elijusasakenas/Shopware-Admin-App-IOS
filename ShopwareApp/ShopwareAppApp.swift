14bb0c86235b594b9d42ac22a367396323711711
elijusasakenas <elijusasakenas@gmail.com>
2026-06-12 00:17:15 +0200

    Initial Commit

---
+++ ShopwareApp/ShopwareAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopwareAppApp.swift
+//  ShopwareApp
+//
+//  Created by Elijus Asakenas on 12/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct ShopwareAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
